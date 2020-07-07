81088f97135bbf025519c84905bd0f3f7d1430cf
HRX <2943068628@qq.com>
2020-07-07 11:28:48 +0800

    将untitled工程添加到版本库

---
+++ untitled/src/Main.java
@@ -0,0 +1,21 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i =1;
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("请输入一个数字");
+        int r=sc.nextInt();
+        if(i==r){
+            System.out.println("你好");
+        }else {
+            System.out.println("hello");
+        }
+    }
+}
+
+
